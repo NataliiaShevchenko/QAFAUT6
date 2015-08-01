d27012c62daab0cfacd93d2a2cc5fa3c15e17298
Nataliia <dnv2605@gmail.com>
2015-08-01 12:08:17 +0300

    домашка3(исправления)

--- 7/src/com/company/Main.java
+++ 7/src/com/company/Main.java
@@ -50,24 +50,47 @@ public class Main {
         int[] operand2 = {2, 63, 33, 91, 98, 20, 95, 95, 5, 83, 38, 66, 87};
         int[] expectedResults = {98, 98, 100, 137, 185, 96, 180, 125, 99, 91, 126, 148, 90};
 
-        boolean isPassed = true;
+        boolean testresult = true;
 
         for (int i=0; i<operand1.length; i++) {
             int sum=operand1[i] + operand2[i];
             int expected=expectedResults[i];
             if (sum != expected) {
                 System.out.println("Expected: "+ expected +"; Actual: "+sum);
-                isPassed = false;
+                testresult = false;
             }
         }
 
-        if (isPassed == true)
+        if (testresult == true)
         {
             System.out.print("Tests PASSED");
         }
         else{
             System.out.print("Tests FAILED");
         }
+
+        //TARGETS
+        //5
+        int[] notstrings = {1, 2, 3, 4, 1, 3, 1, 6, 6, 1, 1};
+        int target = 1;
+        int result =0;
+        for (int i=0; i<11; i++){
+            if (notstrings[i]==target){
+                result++;
+                System.out.println(result);
+
+            }
+        }
+
+        //2
+        //int[] notstrings = {1, 2, 3, 4, 1, 3, 1, 6, 6, 1, 1};
+        //int target = 6;
+
+        //3
+        //int[] notstrings = {1, 7, 1, 4, 1, 7, 9, 6, 13, 1, 7};
+        //int target = 7;
+
+
 
 
 
